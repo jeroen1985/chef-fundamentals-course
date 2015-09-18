6eb856a6c7777901f111092087a69f332f5bf492
Jeroen Stevens <jeroenstevens@Tais-MacBook-Pro.local>
2015-09-17 18:10:02 -0700

    day2 of Chef training

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,6 @@
+default["apache"]["indexfile"] = "index.html"
+
+default["apache"]["sites"]["clowns"] = { "port" => 80, "nose" => "red" }
+default["apache"]["sites"]["bears"] = { "port" => 81, "nose" => "black" }
+default["apache"]["sites"]["zebras"] = { "port" => 82, "nose" => "zebras" }
+
---
+++ cookbooks/ip-logger/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: ip-logger
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+search("node","ipaddress:10.*").each do |server|
+	log "The servers in your organization have the following FQDN/IP Address:- #{server['fqdn']}/#{server['ipaddress']}"
+end
+
